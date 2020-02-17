96197a88df3ba61faf537c87589828c3e0429cb3
yanxingzhe <yanxingzhe@pinduoduo.com>
2020-02-17 10:23:41 +0800

    fix: revert codesandbox

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Switch,
-  Route
-} from "react-router-dom";
-// import Static from "./Static";
-// import Dynamic from "./Dynamic";
-import Code from "./Code";
-import Test from "./Test";
+import Static from "./Static";
+import Dynamic from "./Dynamic";
 
 const Demo = () => {
   return (
